71d9e206a9cb03a04d3f743fa0f5f3a8d7d8eadb
Ky1inZhang <a1179112978@gmail.com>
2020-03-28 14:10:03 +0800

    no message

--- msgDB.py
+++ msgDB.py
@@ -1,6 +1,9 @@
 import sqlite3
 import time
+import json
+import urllib.request
 
+# https://github.com/Ky1inZhang/WechatBot     api文档
 #底层通信
 def initDB():
       global conn
@@ -38,19 +41,63 @@ def listen_wxMsg():
             #print(recMsg()[0])
             return res[0]
       else:
-            return False
+            return "False"
+
+
+
+# 图灵请求回复
+def getMsg(msg):
+    api_url = "http://openapi.tuling123.com/openapi/api/v2"
+    text_input = msg
+
+    req = {
+        "perception":
+        {
+            "inputText":
+            {
+                "text": text_input
+            },
+        },
+        "userInfo": 
+        {
+            "apiKey": "17abd15ba99e460a8b8cc2d433e97df9",
+            "userId": "OnlyUseAlphabet"
+        }
+    }
+    # print(req)
+    # 将字典格式的req编码为utf8
+    req = json.dumps(req).encode('utf8')
+    # print(req)
+
+    http_post = urllib.request.Request(api_url, data=req, headers={'content-type': 'application/json'})
+    response = urllib.request.urlopen(http_post)
+    response_str = response.read().decode('utf8')
+    # print(response_str)
+    response_dic = json.loads(response_str)
+    # print(response_dic)
+
+    intent_code = response_dic['intent']['code']
+    results_text = response_dic['results'][0]['values']['text']
+    # print('Turing的回答：')
+    # print('code：' + str(intent_code))
+    # print('text：' + results_text)
+    print(results_text)
+    return results_text
+
+
 
 if __name__=="__main__":
       initDB()#初始化
-      for i in range(1000):
-            delMsg()#清空数据库
+      # for i in range(1000):
+      #       delMsg()#清空数据库
       while True: #死循环
             res=listen_wxMsg()#监听一次是否有新消息（本质是查询一次数据库是否有记录）
             if res==False:#无新消息产生则开始下一轮监听
                   continue
-            if res[3]=="111":#res[3]存着别人发来的消息
-                  print(res[0])
-                  send_wxMsg(res[0],"response")#res[0]是发送消息的人的id
+            if res[0]=="wxid_c5du2oo2mwhi12":#res[3]存着别人发来的消息
+                  print(res[0] )
+                  response = getMsg(res[3])
+                  send_wxMsg(res[0],response)#res[0]是发送消息的人的id
             if res[3]=="picture":
                   print(res[0])
                   send_wxPicture(res[0],"C:\\1.jpg")#发送图片的函数，必须传入本地图片路径，不能是链接
